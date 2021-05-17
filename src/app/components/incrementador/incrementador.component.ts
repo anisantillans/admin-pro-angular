26293ba27805cefdc29bbfbf9484e6b1732c5c1c
Ana Paula Santillan <ana.santillan@monkeyfit.co>
2021-05-17 10:34:18 -0500

    added incrementador and progress component

---
+++ src/app/components/incrementador/incrementador.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-incrementador',
+  templateUrl: './incrementador.component.html',
+  styles: [],
+})
+export class IncrementadorComponent {
+  //enviar valor del padre al hijo
+  @Input('valor') progreso: number = 50;
+  @Input() btnClass: string = 'btn btn-primary';
+  @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
+
+  cambiarValor(valor: number) {
+    if (this.progreso >= 100 && valor >= 0) {
+      this.valorSalida.emit(100);
+      return (this.progreso = 100);
+    }
+    if (this.progreso <= 100 && valor < 0) {
+      this.valorSalida.emit(0);
+      return (this.progreso = 0);
+    }
+    this.progreso = this.progreso + valor;
+    this.valorSalida.emit(this.progreso);
+  }
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ComponentsModule } from '../components/components.module';
 //Components
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
@@ -22,6 +24,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     Grafica1Component,
     PagesComponent,
   ],
-  imports: [CommonModule, SharedModule, RouterModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule,
+    FormsModule,
+    ComponentsModule,
+  ],
 })
 export class PagesModule {}
