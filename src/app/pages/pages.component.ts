0d3469ff0c43a4b6af7ebf4010434dbc0908c411
Ana Paula Santillan <ana.santillan@monkeyfit.co>
2021-05-22 23:32:51 -0500

    Fin seccion 7

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
+declare function customInitFunctions();
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -8,5 +9,7 @@ import { SettingsService } from '../services/settings.service';
 })
 export class PagesComponent implements OnInit {
   constructor(private settingsService: SettingsService) {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    customInitFunctions();
+  }
 }
