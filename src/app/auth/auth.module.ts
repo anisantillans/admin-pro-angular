54df5d70b57233e64d85118d8107f81adf773746
Ana Paula Santillan <ana.santillan@monkeyfit.co>
2021-05-28 10:29:02 -0500

    added usuario model and fixed login and register components

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
   exports: [RegisterComponent, LoginComponent],
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
 })
 export class AuthModule {}
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['../login/login.component.css'],
 })
-export class RegisterComponent implements OnInit {
-  constructor() {}
+export class RegisterComponent {
+  public formSubmitted = false;
 
-  ngOnInit(): void {}
+  public registerForm = this.fb.group({
+    nombre: ['Fernando', Validators.required],
+    email: ['test123@gmail.com', [Validators.required, Validators.email]],
+    password: ['123456', Validators.required],
+    password2: ['123456', Validators.required],
+    terminos: [false, Validators.required],
+  });
+  constructor(private fb: FormBuilder) {}
+
+  crearUsuario() {
+    this.formSubmitted = true;
+    console.log(this.registerForm.value);
+    if (this.registerForm.valid) {
+      console.log('posteando formulario');
+    } else {
+      console.log('formulario no es correcto...');
+    }
+  }
+  campoNoValido(campo: string): boolean {
+    if (this.registerForm.get(campo).invalid && this.formSubmitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  aceptaTerminos() {
+    return !this.registerForm.get('terminos').value && this.formSubmitted;
+  }
 }
